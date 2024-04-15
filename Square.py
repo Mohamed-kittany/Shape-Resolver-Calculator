3255bae1cd81ae78814f5367531467173555b41e
Mohamed-kittany <161580792+Mohamed-kittany@users.noreply.github.com>
2024-04-16 00:24:54 +0300

    Update Square.py

--- Square.py
+++ Square.py
@@ -1,11 +1,10 @@
 # !/usr/bin/env python3
 from Rectangle import Rectangle
 
-# Define a subclass Square inheriting from Rectangle
 class Square(Rectangle):
-    def __init__(self, side):
-        super().__init__(side, side)  # Call the superclass constructor
+    def __init__(self, side: float):
+        super().__init__(side, side)
 
-    # Override the get area method
-    def __str__(self):
-        return f"The square area is: {self.get_area()}"
+    def __str__(self) -> str:
+        return f"The Square area is: {self.get_area():.2f}"
+
