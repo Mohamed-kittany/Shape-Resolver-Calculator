b06d2d553316de108e366f79b8c625d531ddd260
Mohamed-kittany <161580792+Mohamed-kittany@users.noreply.github.com>
2024-04-16 00:24:12 +0300

    Update Rectangle.py

--- Rectangle.py
+++ Rectangle.py
@@ -1,14 +1,13 @@
 # !/usr/bin/env python3
 from Shape import Shape
 
-
 class Rectangle(Shape):
-    def __init__(self, side1, side2):
-        super().__init__(side1, side2)  # Call the superclass constructor
+    def __init__(self, width: float, height: float):
+        self.width = self.validate_positive_number(width, "Width")
+        self.height = self.validate_positive_number(height, "Height")
 
-    def get_area(self):
-        return self.side1 * self.side2
+    def get_area(self) -> float:
+        return self.width * self.height
 
-    # Override the get area method
-    def __str__(self):
-        return f"The Rectangle area is: {self.get_area()}"
+    def __str__(self) -> str:
+        return f"The Rectangle area is: {self.get_area():.2f}"
