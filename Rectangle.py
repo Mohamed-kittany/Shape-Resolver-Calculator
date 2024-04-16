7edadfe8a9f58b3a47c19ebec2e95600c950c021
ElhamAbedAlkareem <34744131+ElhamAbedAlkareem@users.noreply.github.com>
2024-04-16 15:24:40 +0300

    Added Rectangle Class

--- Rectangle.py
+++ Rectangle.py
@@ -4,11 +4,18 @@ from Shape import Shape
 
 class Rectangle(Shape):
     def __init__(self, side1, side2):
+        if side1 <= 0 or side2 <= 0:
+            raise ValueError("Side lengths of Rectangle must be positive.")
         super().__init__(side1, side2)  # Call the superclass constructor
-
+    
+    # Override the gget_area method
     def get_area(self):
         return self.side1 * self.side2
+  
+    # Override the get_perimeter method
+    def get_perimeter(self):
+        return (2 * self.side1 + 2 * self.side2)
 
-    # Override the get area method
+    # Override the str method
     def __str__(self):
-        return f"The Rectangle area is: {self.get_area()}"
+        return f"The Rectangle area is: {self.get_area()} , The Rectangle perimeter is: {self.get_perimeter()}"
