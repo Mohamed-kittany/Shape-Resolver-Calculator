299cc3a25256ad78be7d75fbe15ff1377312973e
ElhamAbedAlkareem <34744131+ElhamAbedAlkareem@users.noreply.github.com>
2024-04-16 15:31:13 +0300

    Added Hexagon Class

--- Hexagon.py
+++ Hexagon.py
@@ -1,14 +1,22 @@
 # !/usr/bin/env python3
-from Rectangle import Rectangle
+from Square import Square
 from math import sqrt
 
 
-class Hexagon(Rectangle):
+class Hexagon(Square):
     def __init__(self, side1):
-        super().__init__(side1, side1)  # Call the superclass constructor
+        if side1 <= 0 :
+            raise ValueError("Side lengths of Hexagon must be positive.")
+        super().__init__(side1)  # Call the superclass constructor
+    
+    # Override the get_area method
     def get_area(self):
-        return (3 * sqrt(3) * self.side1 ** 2) / 2
+        return (3 * sqrt(3) * (super().get_area())) / 2
+    
+    # Override the get_perimeter method
+    def get_perimeter(self):
+        return (6 * self.side1)
 
-    # Override the get area method
+    # Override the str  method
     def __str__(self):
-        return f"The Hexagon area is: {self.get_area()}"
+        return f"The Hexagon area is: {self.get_area()},The Hexagon perimeter is: {self.get_perimeter()}"
