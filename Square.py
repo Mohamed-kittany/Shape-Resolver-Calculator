d6e0f2d59f3d91d66314dfd03c47e235d207cbd5
ElhamAbedAlkareem <34744131+ElhamAbedAlkareem@users.noreply.github.com>
2024-04-16 15:22:44 +0300

    Added Square Class

--- Square.py
+++ Square.py
@@ -4,8 +4,11 @@ from Rectangle import Rectangle
 # Define a subclass Square inheriting from Rectangle
 class Square(Rectangle):
     def __init__(self, side):
-        super().__init__(side, side)  # Call the superclass constructor
+        if side <= 0:
+            raise ValueError("Side lengths of Square must be positive.")
+        super().__init__(side , side)  # Call the superclass constructor
 
-    # Override the get area method
+    # Override the str method
     def __str__(self):
-        return f"The square area is: {self.get_area()}"
+        return f"The square area is: {self.get_area()} , The square perimeter is: {self.get_perimeter()}"
+   
