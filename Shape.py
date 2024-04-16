a21ce4e7fee3fe011f43026fa98b48080e431bf4
ElhamAbedAlkareem <34744131+ElhamAbedAlkareem@users.noreply.github.com>
2024-04-16 15:23:44 +0300

    Added Shape Class

--- Shape.py
+++ Shape.py
@@ -1,16 +1,23 @@
 # !/usr/bin/env python3
 from abc import ABC, abstractmethod
 
-
+#base class for 2D shapes.
 class Shape(ABC):
     def __init__(self, side1, side2):
         self.side1 = side1
         self.side2 = side2
-
+    
+    #Calculates the area of the shape.
     @abstractmethod
     def get_area(self):
         pass
+        
+    #Calculates the perimeter of the shape.
+    @abstractmethod
+    def get_perimeter(self):
+        pass
 
+    #Returns a string represent the shape.
     @abstractmethod
     def __str__(self):
         pass
